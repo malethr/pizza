23c07aefa4e96f7fc7caf613a93d620a5b36ef1a
Maria Thomas <malethr@gmail.com>
2017-07-21 14:40:43 -0700

    replace object name from Order to Pizza

--- js/scripts.js
+++ js/scripts.js
@@ -17,31 +17,31 @@ var checkSize = function (size) {
   return sumOfToppings
 }
 
-function Order (crust, size, toppings) {
+function Pizza (crust, size, toppings) {
   this.pizzaCrust = crust;
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
 }
 
-Order.prototype.getPrice = function () {
+Pizza.prototype.getPrice = function () {
   return this.pizzaCrust + this.pizzaSize + this.pizzaToppings;
 }
 
 // UI Logic
 $(document).ready(function(){
-  $(".pizza-form").submit(function(event){
+  $("#pizza-form").submit(function(event){
     event.preventDefault();
     var inputCrust = parseInt($("select#pizza-crust").val());
     var inputSize = parseInt($("select#pizza-size").val());
     $("input:checkbox[name=pizza-toppings]:checked").each(addToppings);
     var inputToppings = checkSize(inputSize);
-    console.log(inputSize);
-    console.log(inputCrust);
-    console.log(inputToppings);
-
-    var newOrder = new Order(inputCrust, inputSize, inputToppings);
-
-    var price = (newOrder.getPrice()).toFixed(2);
-    console.log(price);
+    var newPizza = new Pizza(inputCrust, inputSize, inputToppings);
+    var price = (newPizza.getPrice()).toFixed(2);
+    $("span#crust-price").text(inputCrust.toFixed(2));
+    $("span#size-price").text(inputSize.toFixed(2));
+    $("span#toppings-price").text(inputToppings.toFixed(2));
+    $("span#total").text(price);
+    $(".pizza-summary").show();
+    $("form#pizza-form").hide();
   });
 });
